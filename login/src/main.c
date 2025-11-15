95332fd601ba4aaf65c104f4cd4746a5b805b5b1
42Paolo <emailcreatasulmomentoo@gmail.com>
2025-11-15 16:46:04 +0100

    fatto

--- login/src/main.c
+++ login/src/main.c
@@ -1,74 +1,74 @@
 #include "utils.h"
-#include <cstdio>
 #include <sqlite3.h>
+#include <string.h>
 #include <stdio.h>
 
-int insert_user(sqlite3 *db, char *username, char *psw)
+
+int insert_user(sqlite3 *db, char *username, char *password)
 {
-    int rc;
-    sqlite3_stmt *stmt;
+	int rc;
+	sqlite3_stmt *stmt; 
+	char *sql = "INSERT INTO user_login (username, password) VALUES ( ? , ? );";
 
-    const char *sql =
-        "INSERT INTO user_login (username, password) VALUES (?, ?);";
+	if((rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL)) != SQLITE_OK)
+	{
+		fprintf(stderr, "ERROR PREPARING QUERY: %s\n", sqlite3_errmsg(db));
+		return (rc);
+	}
 
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "ERROR PREPARING QUERY: %s\n", sqlite3_errmsg(db));
-        return rc;
-    }
-    printf("QUERY PREPARED CORRECTLY\n");
-    
 	sqlite3_bind_text(stmt, 1, username, -1, SQLITE_TRANSIENT);
-    sqlite3_bind_text(stmt, 2, psw, -1, SQLITE_TRANSIENT);
+	sqlite3_bind_text(stmt, 2, password, -1, SQLITE_TRANSIENT);
 
-    rc = sqlite3_step(stmt);
-    if (rc != SQLITE_DONE) {
-        fprintf(stderr, "ERROR EXECUTING QUERY: %s\n", sqlite3_errmsg(db));
-        sqlite3_finalize(stmt);
-        return rc;
-    }
-
-    printf("QUERY EXECUTED CORRECTLY\n");
-
-    sqlite3_finalize(stmt);
-    return SQLITE_OK;
+	if((rc = sqlite3_step(stmt)) != SQLITE_DONE)
+	{
+		fprintf(stderr, "ERROR FILLING QUERY: %s\n", sqlite3_errmsg(db));
+		sqlite3_finalize(stmt);
+		return rc;
+	}
+	sqlite3_finalize(stmt);
+	return SQLITE_OK;
 }
 
 int main(void)
 {
 	sqlite3 *handle_db;
-	char *sql_cmnd;
-	int f_open_db;
 	int rc;
-	int err_msg;
+	char *sql;
+	int flags;
+	char username[100];
+	char psw[100];
 
-	f_open_db = SQLITE_OPEN_CREATE
-				| SQLITE_OPEN_READWRITE;
-	if((rc = sqlite3_open_v2("login.db", &handle_db, f_open_db, NULL)) != SQLITE_OK)
+	flags = SQLITE_OPEN_CREATE|
+			SQLITE_OPEN_READWRITE;
+	if((rc = sqlite3_open_v2("login.db", &handle_db, flags, NULL)) != SQLITE_OK)
 	{
-		fprintf(stderr, "ERROR OPENING DB: %s\n", sqlite3_errmsg(handle_db));
+		fprintf(stderr, "ERROR OPEN DB FILE: %s\n", sqlite3_errmsg(handle_db));
 		sqlite3_close(handle_db);
-		return (rc);
+		return rc;
 	}
-	fprintf(stdout, "DB OPENED CORRECTLY \n");
-	sql_cmnd = "CREATE TABLE IF NOT EXISTS user_login ("
-				"id INTEGER PRIMARY KEY AUTOINCREMENT,"
-				"username TEXT NOT NULL,"
-				"password TEXT NOT NULL"
-				");";
-	if((rc = sqlite3_exec(handle_db, sql_cmnd, NULL, NULL, NULL)) != SQLITE_OK)
+	sql =	"CREATE TABLE IF NOT EXISTS user_login ("
+			"id INTEGER PRIMARY KEY AUTOINCREMENT,"
+			"username TEXT NOT NULL,"
+			"password TEXT NOT NULL);";
+	if((rc = sqlite3_exec(handle_db, sql, NULL, NULL, NULL)) != SQLITE_OK)
 	{
 		fprintf(stderr, "ERROR CREATE TABLE: %s\n", sqlite3_errmsg(handle_db));
 		sqlite3_close(handle_db);
-		return (rc);
+		return rc;
 	}
-	fprintf(stdout, "TABLE CREATED CORRECTLY\n");
-	if((rc = insert_user(handle_db, "paolo", "prova")) != SQLITE_OK)
+	printf("Insert your username: ");
+	fgets(username, sizeof(username), stdin);
+	username[strcspn(username, "\n")] = 0;
+
+	printf("Insert your password: ");
+	fgets(psw, sizeof(psw), stdin);
+	psw[strcspn(psw, "\n")] = 0;
+	if((rc = insert_user(handle_db, username, psw)) != SQLITE_OK)
 	{
 		fprintf(stderr, "ERROR INSERTING DATAS: %s\n", sqlite3_errmsg(handle_db));
 		sqlite3_close(handle_db);
 		return rc;
 	}
-	fprintf(stdout, "DATA INSERT CORRECTLY\n");
+	sqlite3_close(handle_db);
 	return 0;
 }
